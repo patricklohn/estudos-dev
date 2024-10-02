b0d78ea2cf9e54c562f09df59dd8d53d71734d43
Patrick Lohn <patricklohn3@gmail.com>
2024-10-02 00:05:17 -0300

    ajuste geral git2

--- Front-And/ReactJS/11_ContDown/countdown/src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Title.css';
-
-const Title = ({title}) => {
-  return <h1 className='title'>{title}</h1>;
-}
-
-export default Title
--- Front-And/ReactJS/2_FUNDAMENTOS/Fundamentos/src/components/MyComponent.jsx
+++
@@ -1,9 +0,0 @@
-export const MyComponent = () => {
-  return (
-    <div>  
-        <p>Estou em outros componentes</p>
-    </div>
-  )
-}
-
-export default MyComponent;
